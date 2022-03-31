7d3c5a7477b74fd5d44562970be5571957b9716d
Angelica Lemos <angelica.lemos@betternow.com.br>
2022-03-30 21:29:38 -0300

    inclusao da pasta fixtures e alteração no mongo

--- rocklov/features/step_definitions/anuncios_steps.rb
+++ rocklov/features/step_definitions/anuncios_steps.rb
@@ -1,4 +1,5 @@
 Dado('que estou logado como {string} e {string}') do |email, password|
+  @email = email
   visit "/"
   find("input[placeholder='Seu e-email']").set email
   find('input[type=password]').set password
@@ -14,18 +15,23 @@ end
 
 Dado('que eu tenho o seguinte equipamento:') do |table|
   @anuncio = table.rows_hash
-  log @anuncio
+
+  MongoDB.new.remove_equipo(@anuncio[:nome], @email)
 end
 
 Quando('submeto o cadastro desse item') do
+  thumb = Dir.pwd + '/features/support/fixtures/images/' + @anuncio[:thumb]
+
+  find('#thumbnail input[type=file]', visible: false).set thumb
   find('input[placeholder$=equipamento]').set @anuncio[:nome]
   find('#category').find('option', text: @anuncio[:categoria]).select_option
   find('#price').set @anuncio[:preco]
 
-  #click_button 'Cadastrar'
-  sleep(10) #temporario
+  click_button 'Cadastrar'
 end
 
 Entao('devo ver esse item no meu Dashboard') do
-  pending # Write code here that turns the phrase above into concrete actions
+  anuncios = find('.equipo-list')
+  expect(anuncios).to have_content @anuncio[:nome]
+  expect(anuncios).to have_content "R$#{@anuncio[:preco]}/dia"
 end
