63b15d72f4ba76a8e627af4599ce4b066843182c
angelLemos <57679682+angelLemos@users.noreply.github.com>
2022-06-24 12:16:32 -0300

    Delete rocklov directory

--- rocklov/features/step_definitions/login_steps.rb
+++
@@ -1,7 +0,0 @@
-Dado('que acesso a página principal') do
-  @login_page.acessar_site
-end
-
-Quando('submeto minhas credenciais com {string} e {string}') do |email, password|
-  @login_page.preencher_dados_login(email, password)
-end
--- rocklov/features/support/views/components/alert.rb
+++
@@ -1,9 +0,0 @@
-
-
-class Alert
-  include Capybara::DSL
-
-  def alert_dark
-    return find('.alert-dark').text
-  end
-end
