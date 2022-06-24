9325cb04c3a0d15c126f1bb14cc28592017e5ac0
Angelica Lemos <angelica.lemos@betternow.com.br>
2022-06-24 11:45:36 -0300

    Inclusão da função multi-browser e diferentes ambientes

--- rocklov/features/support/env.rb
+++ rocklov/features/support/env.rb
@@ -6,10 +6,25 @@ require 'faker'
 require 'pry'
 require 'allure-cucumber'
 
+CONFIG = YAML.load_file(File.join(Dir.pwd, "features/support/config/#{ENV["CONFIG"]}"))
+
+case ENV['BROWSER']
+when 'firefox'
+  @driver = :selenium
+when 'chrome'
+  @driver = :selenium_chrome
+when 'fire_headless'
+  @driver = :selenium_headless
+when 'chrome_headless'
+  @driver = :selenium_chrome_headless
+else
+  raise 'Navegador incorreto, variável @driver indefinida!'
+end
+
 Capybara.configure do |config|
-  config.default_driver = :selenium_chrome
+  config.default_driver = @driver
   Capybara.page.driver.browser.manage.window.maximize
-  config.app_host = 'http://rocklov-web:3000'
+  config.app_host = CONFIG['url']
   config.default_max_wait_time = 10
 end
 
