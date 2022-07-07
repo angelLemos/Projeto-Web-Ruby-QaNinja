e6c2ce5ac4d2076789bcd284341ea69e88b39073
Angelica Lemos <angelica.lemos@betternow.com.br>
2022-07-07 18:35:20 -0300

    Inclusão de cenários

--- api/spec/post_signup_spec.rb
+++ api/spec/post_signup_spec.rb
@@ -18,4 +18,56 @@ describe 'POST /signup' do
       expect(@result.parsed_response['_id'].length).to eql 24
     end
   end
+
+  context 'usuario ja existe' do
+    before(:all) do
+      #Dado possuir um novo usuario
+      payload = { name: 'Melissa', email: 'melissa@live.com.br', password: 'pwd123' }
+      MongoDB.new.remove_user(payload[:email])
+
+      #E inserir o email desse usuário que já foi cadastrado no sistema
+      Signup.new.create(payload)
+
+      #Quando fazer uma requisição para a rota /signup
+      @result = Signup.new.create(payload)
+    end
+
+    it 'deve retornar 409' do
+
+      #Então validar que o retorno é 409
+      expect(@result.code).to eql 409
+    end
+
+    it 'deve retornar mensagem' do
+      expect(@result.parsed_response['error']).to eql 'Email already exists :('
+    end
+  end
+
+  context 'validar campos obrigatorios' do
+    before(:all) do
+      payload_sem_name = { name: '', email: 'melissa@live.com.br', password: 'pwd123' }
+      payload_sem_email = { name: 'Melissa', email: '', password: 'pwd123' }
+      payload_sem_password = { name: 'Melissa', email: 'melissa@live.com.br', password: '' }
+
+      @sem_name = Signup.new.create(payload_sem_name)
+      @sem_email = Signup.new.create(payload_sem_email)
+      @sem_password = Signup.new.create(payload_sem_password)
+
+    end
+
+    it 'tentativa de cadastro sem name deve retornar mensagem de aviso' do
+      expect(@sem_name.code).to eql 412
+      expect(@sem_name.parsed_response['error']).to eql 'required name'
+    end
+
+    it 'tentativa de cadastro sem email deve retornar mensagem de aviso' do
+      expect(@sem_email.code).to eql 412
+      expect(@sem_email.parsed_response['error']).to eql 'required email'
+    end
+
+    it 'tentativa de cadastro sem password deve retornar mensagem de aviso' do
+      expect(@sem_password.code).to eql 412
+      expect(@sem_password.parsed_response['error']).to eql 'required password'
+    end
+  end
 end
